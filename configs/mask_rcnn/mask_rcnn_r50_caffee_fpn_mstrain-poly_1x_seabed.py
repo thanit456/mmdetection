0108df584458f6e824811539c9a25cf491f71343
Thanit Tativannarat <too_w_o@hotmail.com>
2021-01-01 23:58:58 +0700

    move seabed config into mask_rcnn

--- configs/mask_rcnn/mask_rcnn_r50_caffee_fpn_mstrain-poly_1x_seabed.py
+++ configs/mask_rcnn/mask_rcnn_r50_caffee_fpn_mstrain-poly_1x_seabed.py
@@ -1,5 +1,5 @@
 # The new config inherits a base config to highlight the necessary modification
-_base_ = 'mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
+_base_ = 'mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
 
 # We also need to change the num_classes in head to match the dataset's annotation
 model = dict(
