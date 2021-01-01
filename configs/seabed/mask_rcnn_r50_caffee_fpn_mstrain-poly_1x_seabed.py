c46752c3b397f62adcaedb0673f1cfe92e863575
Thanit Tativannarat <too_w_o@hotmail.com>
2021-01-01 22:29:39 +0700

    added sedbed config

---
+++ configs/seabed/mask_rcnn_r50_caffee_fpn_mstrain-poly_1x_seabed.py
@@ -0,0 +1,28 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = 'mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=3),
+        mask_head=dict(num_classes=3)))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('point', 'line', 'scatter')
+data = dict(
+    train=dict(
+        img_prefix='seabed/',
+        classes=classes,
+        ann_file='seabed/all.json'),
+    val=dict(
+        img_prefix='seabed/',
+        classes=classes,
+        ann_file='seabed/all.json'),
+    test=dict(
+        img_prefix='seabed/',
+        classes=classes,
+        ann_file='seabed/all.json'))
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+load_from = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
