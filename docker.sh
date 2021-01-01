4537f4f96553b937bef595788e5f76ff552d5103
thanit456 <too_w_o@hotmail.com>
2021-01-01 23:54:49 +0700

    adjust dockerfile to refer to my forked repo and create an easy script for using docker

--- docker.sh
+++ docker.sh
@@ -1,1 +1,4 @@
-docker run --gpus all --shm-size=8g -it -v {DATA_DIR}:/mmdetection/data mmdetection
+docker run --gpus all --shm-size=8g -it \
+    -v /home/thanit456/part-time/jumpai/mmdetection/data:/mmdetection/data \
+    -v /home/thanit456/part-time/jumpai/mmdetection/checkpoints:/mmdetection/checkpoints \ 
+    mmdetection 
