38d150cb6eda7306d8dabc9bd5595a7757c1aff1
thanit456 <too_w_o@hotmail.com>
2021-01-02 09:22:37 +0700

    adjust docker script

--- docker.sh
+++ docker.sh
@@ -1,4 +1,5 @@
 docker run --gpus all --shm-size=8g -it \
-    -v /home/thanit456/part-time/jumpai/mmdetection/data:/mmdetection/data \
-    -v /home/thanit456/part-time/jumpai/mmdetection/checkpoints:/mmdetection/checkpoints \ 
-    mmdetection 
+-v /home/thanit456/part-time/jumpai/mmdetection/data:/mmdetection/data \
+-v /home/thanit456/part-time/jumpai/mmdetection/checkpoints:/mmdetection/checkpoints \
+-v /home/thanit456/part-time/jumpai/mmdetection/work_dirs:/mmdetection/work_dirs \
+mmdetection 
